37d2a5b869e906eeb147c3dbd64757dbbbdad784
SchaapGuido <56918622+SchaapGuido@users.noreply.github.com>
2021-02-04 12:45:30 +0100

    Delete InstallIrfan7zip.ps1

--- InstallIrfan7zip.ps1
+++
@@ -1,4 +0,0 @@
-Write-Host '*** WVD AIB CUSTOMIZER PHASE *** INSTALL *** Install 7-zip ***'
-Invoke-WebRequest -Uri 'https://www.7-zip.org/a/7z1900-x64.msi' -OutFile 'c:\temp\7z1900-x64.msi'
-Invoke-Expression -Command 'msiexec /i c:\temp\7z1900-x64.msi /quiet'
-Write-Host '*** WVD AIB CUSTOMIZER PHASE *** INSTALL *** Install 7-zip *** - Exit Code: ' $LASTEXITCODE
