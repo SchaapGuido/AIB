cc83c91b8849b17825e61274f4a60ec18fcb837f
Guido Schaap <schaap@dhd.nl>
2021-02-05 11:07:32 +0100

    Added remote installs

--- GuidoTest.ps1
+++ GuidoTest.ps1
@@ -14,10 +14,38 @@ Write-Host '*** WVD AIB CUSTOMIZER PHASE *** CONFIG *** End set locale *** - Exi
 
 Start-Sleep -Seconds 10
 
-Write-Host '*** WVD AIB CUSTOMIZER PHASE *** CONFIG *** Begin set locale ***'
+Write-Host '*** WVD AIB CUSTOMIZER PHASE *** CONFIG *** Begin install language pack ***'
 Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/SchaapGuido/AIB/main/Microsoft-Windows-Client-Language-Pack_x64_nl-nl.cab' `
     -OutFile 'C:\temp\Microsoft-Windows-Client-Language-Pack_x64_nl-nl.cab'
-Add-WindowsPackage -Online -PackagePath 'C:\temp\Microsoft-Windows-Client-Language-Pack_x64_nl-nl.cab'
-Write-Host '*** WVD AIB CUSTOMIZER PHASE *** CONFIG *** End set locale *** - Exit Code: ' $LASTEXITCODE
+Add-WindowsPackage -Online -PackagePath 'C:\temp\Microsoft-Windows-Client-Language-Pack_x64_nl-nl.cab' | Out-Null
+Write-Host '*** WVD AIB CUSTOMIZER PHASE *** CONFIG *** End install language pack *** - Exit Code: ' $LASTEXITCODE
+
+Write-Host '*** WVD AIB CUSTOMIZER PHASE *** CONFIG *** Begin set WinUserLanguageList ***'
+$LanguageList = Get-WinUserLanguageList
+$LanguageList.Add("nl-nl")
+Set-WinUserLanguageList $LanguageList -force
+Write-Host '*** WVD AIB CUSTOMIZER PHASE *** CONFIG *** Begin set WinUserLanguageList ***'
+
+Write-Host '*** WVD AIB CUSTOMIZER PHASE *** CONFIG *** Download latest Office 365 ***'
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/SchaapGuido/AIB/main/setup.exe' -OutFile 'c:\temp\setup.exe'
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/SchaapGuido/AIB/main/Config.xml' -OutFile 'c:\temp\Config.xml'
+Start-Sleep -Seconds 10
+Invoke-Expression -Command 'C:\temp\setup.exe /download c:\temp\config.xml'
+Write-Host '*** WVD AIB CUSTOMIZER PHASE *** CONFIG *** Download latest Office 365 ***'
+
+Start-Sleep -Seconds 30
+
+Write-Host '*** WVD AIB CUSTOMIZER PHASE *** CONFIG *** Install latest Office 365 ***'
+Invoke-Expression -Command 'C:\temp\setup.exe /configure c:\temp\config.xml'
+Start-Sleep -Seconds 30
+Write-Host '*** WVD AIB CUSTOMIZER PHASE *** CONFIG *** Install latest Office 365 *** - Exit Code: ' $LASTEXITCODE
+
+Write-Host '*** WVD AIB CUSTOMIZER PHASE *** INSTALL *** Install FSLogix ***'
+# Note: Settings for FSLogix can be configured through GPO's)
+Invoke-WebRequest -Uri 'https://aka.ms/fslogix_download' -OutFile 'c:\temp\fslogix.zip'
+Expand-Archive -Path 'C:\temp\fslogix.zip' -DestinationPath 'C:\temp\fslogix\'  -Force
+Invoke-Expression -Command 'C:\temp\fslogix\x64\Release\FSLogixAppsSetup.exe /install /quiet /norestart'
+Start-Sleep -Seconds 10
+Write-Host '*** WVD AIB CUSTOMIZER PHASE *** INSTALL *** Install FSLogix *** - Exit Code: ' $LASTEXITCODE
 
 Write-Host '*** WVD AIB CUSTOMIZER PHASE ********************* END *************************'
