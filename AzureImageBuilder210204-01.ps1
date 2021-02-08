89790badc3d0ceb6acda2717f71eb750e0e4cf3b
Guido Schaap <schaap@dhd.nl>
2021-02-08 10:20:01 +0100

    removed deletion of temp dir

--- AzureImageBuilder210204-01.ps1
+++ AzureImageBuilder210204-01.ps1
@@ -180,7 +180,7 @@ $ImgCustomParams = @{
   PowerShellCustomizer = $true
   CustomizerName = 'InstallApp'
   RunElevated = $true
-  ScriptUri = "https://raw.githubusercontent.com/SchaapGuido/AIB/main/Win10ms_O365Original.ps1"
+  ScriptUri = "https://raw.githubusercontent.com/SchaapGuido/AIB/main/Win10ms_O365V01.ps1"
 }
 $Customizer = New-AzImageBuilderCustomizerObject @ImgCustomParams
 
