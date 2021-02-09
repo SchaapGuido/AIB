e7728f1e8e8ca58830c3919ffc3d8aaea259dcbb
SchaapGuido <56918622+SchaapGuido@users.noreply.github.com>
2021-02-09 16:14:45 +0100

    Update sysprep.ps1

--- sysprep.ps1
+++ sysprep.ps1
@@ -1,4 +1,4 @@
 $VMLocalAdminUser = "LocalAdminUser"
 $VMLocalAdminSecurePassword = ConvertTo-SecureString "J6%W98Y^rZ" -AsPlainText -Force
 $Credential = New-Object System.Management.Automation.PSCredential ($VMLocalAdminUser, $VMLocalAdminSecurePassword);
-Start-process 'C:\Windows\System32\Sysprep\sysprep.exe /generalize /oobe /shutdown /quiet' -Credential $Credential
+Start-process 'C:\Windows\System32\Sysprep\sysprep.exe /generalize /oobe /quit /quiet' -Credential $Credential
