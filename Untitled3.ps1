9d59fc455bf8b8b439b6a94ef0a8958c67b8ec92
Guido Schaap <schaap@dhd.nl>
2021-02-16 12:55:28 +0100

    remove double entry

---
+++ Untitled3.ps1
@@ -0,0 +1,47 @@
+﻿$imageResourceGroup = 'rg-wvd-ont'
+$location = 'Westeurope'
+$imageTemplateName = 'WVD-Images'
+$runOutputName = 'myDistResults'
+$subscriptionID = (Get-AzContext).Subscription.Id
+$imageRoleDefName = "Azure Image Builder Image Role Definition"
+$identityName = "AzureImageBuilderUserIdentity"
+$myGalleryName = 'AIBImageGallery'
+$imageDefName = 'WVD-Images'
+
+$SrcObjParams = @{
+  SourceTypePlatformImage = $true
+  Publisher = 'MicrosoftWindowsDesktop'
+  Offer = 'office-365'
+  Sku = '20h1-evd-o365pp'
+  Version = 'latest'
+}
+$srcPlatform = New-AzImageBuilderSourceObject @SrcObjParams
+
+$disObjParams = @{
+  SharedImageDistributor = $true
+  ArtifactTag = @{tag='dis-share'}
+  GalleryImageId = "/subscriptions/$subscriptionID/resourceGroups/$imageResourceGroup/providers/Microsoft.Compute/galleries/$myGalleryName/images/$imageDefName"
+  ReplicationRegion = $location
+  RunOutputName = $runOutputName
+  ExcludeFromLatest = $false
+}
+$disSharedImg = New-AzImageBuilderDistributorObject @disObjParams
+
+$ImgCustomParams = @{
+  PowerShellCustomizer = $true
+  CustomizerName = 'InstallApp'
+  RunElevated = $true
+  ScriptUri = "https://raw.githubusercontent.com/SchaapGuido/AIB/main/Win10ms_O365Ov0_3.ps1"
+}
+$Customizer = New-AzImageBuilderCustomizerObject @ImgCustomParams
+
+$ImgTemplateParams = @{
+  ImageTemplateName = $imageTemplateName
+  ResourceGroupName = $imageResourceGroup
+  Source = $srcPlatform
+  Distribute = $disSharedImg
+  Customize = $Customizer
+  Location = $location
+  UserAssignedIdentityId = $identityNameResourceId
+}
+New-AzImageBuilderTemplate @ImgTemplateParams
--- Win10ms_O365v0_3.ps1
+++ Win10ms_O365v0_3.ps1
@@ -52,14 +52,6 @@ Write-Host '*** WVD AIB CUSTOMIZER PHASE *** CONFIG *** Create temp folder for s
 New-Item -Path 'C:\temp' -ItemType Directory -Force | Out-Null
 Write-Host '*** WVD AIB CUSTOMIZER PHASE *** CONFIG *** Create temp folder for software packages. *** - Exit Code: ' $LASTEXITCODE
 
-Write-Host '*** WVD AIB CUSTOMIZER PHASE *** INSTALL *** Install FSLogix ***'
-# Note: Settings for FSLogix can be configured through GPO's)
-Invoke-WebRequest -Uri 'https://aka.ms/fslogix_download' -OutFile 'c:\temp\fslogix.zip'
-Expand-Archive -Path 'C:\temp\fslogix.zip' -DestinationPath 'C:\temp\fslogix\'  -Force
-Invoke-Expression -Command 'C:\temp\fslogix\x64\Release\FSLogixAppsSetup.exe /install /quiet /norestart'
-Start-Sleep -Seconds 10
-Write-Host '*** WVD AIB CUSTOMIZER PHASE *** INSTALL *** Install FSLogix *** - Exit Code: ' $LASTEXITCODE
-
 Write-Host '*** WVD AIB CUSTOMIZER PHASE *** CONFIG *** Install language pack ***'
 ##Disable Language Pack Cleanup##
 Disable-ScheduledTask -TaskPath "\Microsoft\Windows\AppxDeploymentClient\" -TaskName "Pre-staged app cleanup"
