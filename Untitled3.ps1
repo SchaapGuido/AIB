7bec837becb29d3685036e95182ff626f64b944c
Guido Schaap <schaap@dhd.nl>
2021-02-18 11:38:51 +0100

    more changes

--- Untitled3.ps1
+++ Untitled3.ps1
@@ -1,18 +1,21 @@
-﻿$imageResourceGroup = 'rg-wvd-ont'
+﻿Select-AzSubscription -Subscription '92d0bf2b-bd52-4b00-b9b7-5969d1949ba0'
+
+$imageResourceGroup = 'rg-wvd-ont'
 $location = 'Westeurope'
 $imageTemplateName = 'WVD-Images'
 $runOutputName = 'myDistResults'
 $subscriptionID = (Get-AzContext).Subscription.Id
-$imageRoleDefName = "Azure Image Builder Image Role Definition"
 $identityName = "AzureImageBuilderUserIdentity"
 $myGalleryName = 'AIBImageGallery'
 $imageDefName = 'WVD-Images'
 
+$userAssignedIdentity = Get-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $identityName
+
 $SrcObjParams = @{
   SourceTypePlatformImage = $true
   Publisher = 'MicrosoftWindowsDesktop'
-  Offer = 'windows-10'
-  Sku = '20h1-evd'
+  Offer = 'office-365'
+  Sku = '20h2-evd-o365pp'
   Version = 'latest'
 }
 $srcPlatform = New-AzImageBuilderSourceObject @SrcObjParams
@@ -31,7 +34,7 @@ $ImgCustomParams = @{
   PowerShellCustomizer = $true
   CustomizerName = 'InstallApp'
   RunElevated = $true
-  ScriptUri = "https://raw.githubusercontent.com/SchaapGuido/AIB/main/Win10ms_O365v0_3.ps1"
+  ScriptUri = "https://raw.githubusercontent.com/SchaapGuido/AIB/main/Win10ms_O365v0_4.ps1"
 }
 $Customizer = New-AzImageBuilderCustomizerObject @ImgCustomParams
 
@@ -42,13 +45,26 @@ $ImgTemplateParams = @{
   Distribute = $disSharedImg
   Customize = $Customizer
   Location = $location
-  UserAssignedIdentityId = $identityNameResourceId
+  UserAssignedIdentityId = $userAssignedIdentity.Id
 }
 New-AzImageBuilderTemplate @ImgTemplateParams
 
-Get-AzImageBuilderTemplate -ImageTemplateName $imageTemplateName -ResourceGroupName $imageResourceGroup |
+$imageBuilderResult = Get-AzImageBuilderTemplate -ImageTemplateName $imageTemplateName -ResourceGroupName $imageResourceGroup
+
+$imageBuilderResult |
   Select-Object -Property Name, LastRunStatusRunState, LastRunStatusMessage, ProvisioningState, ProvisioningErrorMessage
 
+if ($imageBuilderResult.ProvisioningState -eq "Succeeded")
+{
+  Write-Output "Creating image started."
+  Start-AzImageBuilderTemplate -ResourceGroupName $imageResourceGroup -Name $imageTemplateName
+  Write-Output "Creating image finished."
+}
+else 
+{
+  Write-Warning "Creating template failed!"
+  Write-Warning $imageBuilderResult.ProvisioningErrorMessage
+}
 # Remove-AzImageBuilderTemplate -ImageTemplateName $imageTemplateName -ResourceGroupName $imageResourceGroup
 
-# Start-AzImageBuilderTemplate -ResourceGroupName $imageResourceGroup -Name $imageTemplateName
+# Stop-AzImageBuilderTemplate -ResourceGroupName $imageResourceGroup -Name $imageTemplateName
